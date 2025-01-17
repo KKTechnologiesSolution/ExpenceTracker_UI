c1424d13db76f45c3cb33498ec4ab5df1ea0b8f7
Kannan Krishnan <kannankrishnan1320@gmail.com>
2025-01-17 21:55:39 +0530

    Before ChartNew Branch

--- src/app/Components/dashboard/dashboard.component.ts
+++ src/app/Components/dashboard/dashboard.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component , ViewChild} from '@angular/core';
 import { Route } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatCardModule} from '@angular/material/card';
+import {MatTableModule} from '@angular/material/table';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { ChartComponent } from "ng-apexcharts";
+
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [MatSlideToggleModule,MatCardModule,MatTableModule,MatGridListModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-
+ 
 }
